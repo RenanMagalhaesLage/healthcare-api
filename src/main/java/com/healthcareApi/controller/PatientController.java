edd883fe27ff47820eef5b2081f70df503a0346c
Renan Magalhães <renanlage50@gmail.com>
2025-09-26 10:05:58 -0300

    :sparkles: feat: create patient

---
+++ src/main/java/com/healthcareApi/controller/PatientController.java
@@ -0,0 +1,43 @@
+package com.healthcareApi.controller;
+
+import com.healthcareApi.domain.dto.request.PatientRequestDTO;
+import com.healthcareApi.domain.dto.response.PatientResponseDTO;
+import com.healthcareApi.service.PatientService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/patients")
+@RequiredArgsConstructor
+@Tag(name = "Patients", description = "Endpoints for managing patients")
+public class PatientController {
+    private final PatientService patientService;
+
+    @PostMapping()
+    public ResponseEntity<PatientResponseDTO> create(@RequestBody PatientRequestDTO dto){
+        return ResponseEntity.ok(patientService.create(dto));
+    }
+//    @GetMapping("all")
+//    public ResponseEntity<List<HealthProfessionalResponseDTO>> getAll(@RequestParam Long medicalCenterId){
+//        return ResponseEntity.ok(patientService.getAll(medicalCenterId));
+//    }
+//
+//    @GetMapping("by-specialty")
+//    public ResponseEntity<List<HealthProfessionalResponseDTO>> findBySpecialty(@RequestParam Integer specialty, @RequestParam Long medicalCenterId){
+//        return ResponseEntity.ok(patientService.findBySpecialty(specialty, medicalCenterId));
+//    }
+//
+//    @DeleteMapping()
+//    public ResponseEntity<String> delete(@RequestParam Long healthProfessionalId){
+//        return ResponseEntity.ok(patientService.delete(healthProfessionalId));
+//    }
+//
+//    @PutMapping()
+//    public ResponseEntity<HealthProfessionalResponseDTO> update(@RequestBody HealthProfessionalRequestDTO dto){
+//        return ResponseEntity.ok(patientService.update(dto));
+//    }
+}
